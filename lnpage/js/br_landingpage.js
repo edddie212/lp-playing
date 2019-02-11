8b0ce3da5f0b10d919a13a128582505d6ec709ba
edddie212 <45018910+edddie212@users.noreply.github.com>
2019-02-11 16:36:38 +0200

    Add files via upload

---
+++ lnpage/js/br_landingpage.js
@@ -0,0 +1,6 @@
+
+    window.sr = ScrollReveal();
+    sr.reveal('.container', {duration:1000});
+    sr.reveal('.container-fluid', {duration:1000});
+    sr.reveal('.row', {duration:1000});
+    sr.reveal('.cols3', {duration:1000});
